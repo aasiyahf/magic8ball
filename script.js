227789abde919c25945af92e42eefa12f00fa6a0
Aasiyah Feisal <anfeisal@ncsu.edu>
2018-06-13 14:13:01 -0400

    Repaired functionality of ask button

--- script.js
+++ script.js
@@ -3,33 +3,42 @@ var replies = ["It is certain", "It is decidedly so", "Without a doubt", "Yes de
 
 // Function which refreshes the page in order to reshow the question asked and display an answer
 var generateAns = function() {
-    // create funtion to generate random answer
-    var $answer = $("#Qanswer");
-    var answer = $answer.val();
-    // make ball show answer
-    $(".eightBall").text(answer);
-    // hide description to change it
-    $(".description").hide();
-    // hide other visual parts of header unnecessary to answer
-    $(".line").hide();
-    $(".inputBar").hide();
-    $(".askMe").hide();
+  // create funtion to generate random answer
+  var $answer = $("#Qanswer");
+  var answer = $answer.val();
+  // make ball show answer
+  $(".eightBall").text(answer);
+  // hide description to change it
+  $(".description").hide();
+  // hide other visual parts of header unnecessary to answer
+  $(".line").hide();
+  $(".inputBar").hide();
+  $(".askMe").hide();
 
-    // Generate a random number from 0-19
-    var randomNum = Math.floor(Math.random() * 20);
-    // retrieve answer at index of random number
-    var ballAns = replies[randomNum];
-    // Crate appropriate message if no question was typed
-    if (answer === "") {
-      ballAns = "Reload the page to enter a question."
-    }
+  // Generate a random number from 0-19
+  var randomNum = Math.floor(Math.random() * 20);
+  // retrieve answer at index of random number
+  var ballAns = replies[randomNum];
+  // Crate appropriate message if no question was typed
+  if (answer === "") {
+    ballAns = "Reload the page to enter a question."
+  }
 
-    // Change description to answer message and show in header
-    $(".description").text(ballAns);
-    $(".description").show();
+  // Change description to answer message and show in header
+  $(".description").text(ballAns);
+  $(".description").show();
 };
 
+// // Refresh page and show answer to question asked for magic eight ball
+// $(function(){
+//     $(".askBtn").click(function() {
+//         generateAns();
+//     });
+// });
+
 // Refresh page and show answer to question asked for magic eight ball
-$(".askBtn").on("click", function() {
+$(function(){
+  $(".askBtn").on("click", function() {
     generateAns();
+  });
 });
